f2122a5325ed0846f351769291e15c75d495768a
Serg <borisov.s3@yandex.ru>
2024-07-25 00:25:01 +0300

    upd

--- static/web/blogger-x/BUbPgeSZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./v-5vYYHN.js";import{Y as r,A as a,t as o,S as s}from"./CBm_s1WN.js";import"./CU-ruegT.js";const n={__name:"brief",setup(_){return(e,f)=>(o(),r(t,{theme:"brief"},{default:a(()=>[s(e.$slots,"default")]),_:3}))}};export{n as default};
---
+++ static/web/blogger-x/EI3ynWtG.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./wnzF-fgO.js";import{Y as r,A as a,t as o,S as s}from"./B9HEP0UK.js";import"./BNGmp7kg.js";const n={__name:"brief",setup(_){return(e,f)=>(o(),r(t,{theme:"brief"},{default:a(()=>[s(e.$slots,"default")]),_:3}))}};export{n as default};
